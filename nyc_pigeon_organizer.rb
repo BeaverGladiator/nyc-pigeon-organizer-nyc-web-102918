97d26de0e03f721e7a59ee9b7711952be786f763
Matthew Migliore <mmigliore311@gmail.com>
2018-10-19 15:32:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,11 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   data.each do |attribute, pidgeon_data|
+    pidgeon_data.each do |trait, pidgeons|
+      pidgeons.each.do |pidgeon|
+        binding.pry
+      end 
+    end 
   end
   
   binding.pry
