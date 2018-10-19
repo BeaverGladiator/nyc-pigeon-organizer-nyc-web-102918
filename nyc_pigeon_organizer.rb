bdd6062cd28fe43b38c234540cd3fbc21ece50b9
Matthew Migliore <mmigliore311@gmail.com>
2018-10-19 15:33:39 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_list = {}
+  pidgeon_list = {}
   
   data.each do |attribute, pidgeon_data|
     pidgeon_data.each do |trait, pidgeons|
